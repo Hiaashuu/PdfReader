a65382c0e06960341fb31715155f5b020faeed6d
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-07-29 11:14:52 +0530

    Initial commit via CodXFuse

---
+++ pdf-viewer/src/main/kotlin/com/hiaashuu/pdfreader/source/AssetSource.kt
@@ -0,0 +1,17 @@
+package com.hiaashuu.pdfreader.source
+
+import android.content.Context
+import android.os.ParcelFileDescriptor
+import com.hiaashuu.pdfreader.util.FileUtils
+import com.shockwave.pdfium.PdfDocument
+import com.shockwave.pdfium.PdfiumCore
+import java.io.IOException
+
+class AssetSource(private val assetName: String) : DocumentSource {
+    @Throws(IOException::class)
+    override fun createDocument(context: Context, core: PdfiumCore, password: String?): PdfDocument {
+        val f = FileUtils.fileFromAsset(context, assetName)
+        val pfd = ParcelFileDescriptor.open(f, ParcelFileDescriptor.MODE_READ_ONLY)
+        return core.newDocument(pfd, password)
+    }
+}
